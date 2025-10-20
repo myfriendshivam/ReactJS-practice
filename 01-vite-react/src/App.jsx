711b6393c9659f2894641caa9ab0c2f1f66a27d5
myfriendshivam <shivamkant333@gmail.com>
2025-10-20 13:55:22 +0530

    How react renders elements by mimicking its render process with a custom function that converts JS objects to real DOM, evaluate expression-how to  inject variable and JS in JSX file

--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,8 +1,9 @@
 import Chai  from "./Chai"
 function App() {
+  const username = "Shivam Kant"
   return (
     <>
-   <h1>Chai aur React | Shivam Kant</h1>
+   <h1>Chai aur React | {username}</h1> {/* evaluted expression  means final outcome of JS written here */}
    <Chai/>
    </>
   )
--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,7 +1,42 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | Chai</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// } 
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotheruser = "Shivam Kant"
+
+const reactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target: '_blank'},
+    'click me to visit google',
+    anotheruser
+)
+ 
 createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    // <MyApp/>
+    // ReactElement -> this is not render because your syntax is not correct, the react is written by render function has some expectations which is not fullfill by your reactelement
+    
+    // anotherElement
+    reactElement
 )
